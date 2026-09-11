5600a8d7cdf680ca5aa7f6fdba1fc0259799f520
didier <didiermmp1707@outlook.com>
2026-09-11 12:51:28 -0300

    modelos1.2

--- mini-red social1/Models/Roles.cs
+++ mini-red social1/Models/Roles.cs
@@ -1,12 +1,14 @@
-﻿namespace mini_red_social1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace mini_red_social1.Models;
 
 public class Roles
-{
-    public int id { get; set; }
-    
+{   [Key] public int id { get; set; }
+
     public string nombre { get; set; }
     
-    public string descripcion { get; set; }
+    public string? descripcion { get; set; }
 
     public bool activo { get; set; }
     
--- mini-red social1/Models/Usuario.cs
+++ mini-red social1/Models/Usuario.cs
@@ -1,24 +1,31 @@
-﻿namespace mini_red_social1.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
+namespace mini_red_social1.Models;
+
+[Index(nameof(nombre), IsUnique = true)]
 public class Usuario
 {
     public int id { get; set; }
     
-    public string nombre { get; set; }
+    public int rol_id { get; set; }
+    [ForeignKey(nameof(rol_id))] public Roles role { get; set; } = null!;
     
-    public string apellido { get; set; }
+    public string nombre { get; set; } = string.Empty;
+    
+    public string? apellido { get; set; }
     
     public string correo { get; set; }
     
-    public string descripcion { get; set; }
+    public string? descripcion { get; set; }
 
     public string contrasena_hash { get; set; }
 
     public enum estado { Activo, Inactivo, Pendiente }
-    
     public estado status { get; set; }
 
-    public string visibilidad { get; set; }
+    public enum visibilidad { publico, privado }
+    public visibilidad visiblity { get; set; }
     
     public DateTime creado_en { get; set; }
 
